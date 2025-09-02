8dd8d5b786f25762bf42ac446d4156a0fe51be2a
nufair2006 <nufairsubair2006@gmail.com>
2025-09-02 18:17:29 +0530

    Just Added Some Buggy Code

---
+++ src/main/java/org/springframework/samples/petclinic/model/TestClass.java
@@ -0,0 +1,8 @@
+package org.springframework.samples.petclinic.model;
+
+public class TestClass {
+        public void test() {
+            String s = null;
+            s.toString(); // Null dereference
+        }
+}
