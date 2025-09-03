a72c6e95e760953190a91246f088f19915897fa2
nufair2006 <nufairsubair2006@gmail.com>
2025-09-03 11:32:54 +0530

    Some Modification Done

--- src/main/java/org/springframework/samples/petclinic/model/TestClass.java
+++ src/main/java/org/springframework/samples/petclinic/model/TestClass.java
@@ -5,4 +5,40 @@ public class TestClass {
             String s = null;
             s.toString(); // Null dereference
         }
+
+    Integer i = new Integer(5);  // DM_NUMBER_CTOR (should use Integer.valueOf)
+    int j = i.intValue();
+
+
+    private int counter;
+
+    public synchronized void increment() {
+        counter++;
+    }
+
+    public int getCounter() {  // IS2_INCONSISTENT_SYNC
+        return counter;       // not synchronized
+    }
+
+    public boolean check(Object obj) {
+        return "test".equals(123); // EC_UNRELATED_TYPES
+    }
+
+    public boolean isValid(String s) {
+        return s == "hello"; // ES_COMPARING_STRINGS_WITH_EQ
+    }
+
+    public String buildString() {
+        String result = "";
+        for (int i = 0; i < 100; i++) {
+            result = result + i; // SBSC_USE_STRINGBUFFER_CONCATENATION
+        }
+        return result;
+    }
+
+    private final int[] numbers = {1, 2, 3};
+
+    public int[] getNumbers() {  // EI_EXPOSE_REP
+        return numbers;
+    }
 }
